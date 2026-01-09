b5bb2f9bbbd45b0022731112d283626d5f11c6bd
bune-tea <bntskri2@gmail.com>
2026-01-09 09:49:00 +0900

    Enable nvim-treesitter at java.

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -4,7 +4,7 @@ return {
     build = ":TSUpdate",
     config = function()
         require("nvim-treesitter.configs").setup({
-            ensure_installed = { "c", "lua", "vim", "vimdoc", "python", "ruby", "javascript" },
+            ensure_installed = { "c", "lua", "vim", "vimdoc", "python", "ruby", "javascript", "java" },
             highlight = { enable = true },
         })
     end,
