a6252bba770a943a6b686465e1798fdfc1d5898b
bune-tea <bntskri2@gmail.com>
2026-01-20 21:55:58 +0900

    Add markdown syntaxhilight.

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -5,7 +5,7 @@ return {
     build = ":TSUpdate",
     config = function()
         require("nvim-treesitter.configs").setup({
-            ensure_installed = { "c", "lua", "vim", "vimdoc", "python", "ruby", "javascript", "java" },
+            ensure_installed = { "c", "lua", "vim", "vimdoc", "python", "ruby", "javascript", "java", "markdown", "markdown_inline" },
             highlight = { enable = true },
         })
     end,
